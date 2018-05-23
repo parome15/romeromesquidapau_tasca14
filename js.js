0be1d536583e350d387285e15cceb13a70a960b8
Parome15 <romeromesquidapau@gmail.com>
2018-05-23 17:25:04 +0200

    cambios logica

--- js.js
+++ js.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
     $("#formulari").submit(function(evento){
         //Hem de fer una transformació dels valors del input de dates ja que sino no funcionara ens donara error les dades.
-        var dataInici = new Date($('#dataInici').value());
-        var dataFinal = new Date($('#dataFinal').value());
+        var dataInici = new Date(document.getElementById('#dataInici').value());
+        var dataFinal = new Date(document.getElementById('#dataFinal').value());
         var dataIniciMostrar = dataInici.getDate() +'/'+(dataInici.getMonth()+1)+'/'+dataInici.getFullYear();
         //Aquesta funcio el que fa es donar-nos es dias de diferencia entre les dues fetches.
         function difFechas(dataInici, dataFinal) {
@@ -13,12 +13,12 @@ $(document).ready(function() {
         //Si el valor incial de la data inici es menor a la data final es mostrara la informació del curs,
         // en cas contrari donara error. I sortira un missatge per mostrar la equivocacio de dades.
         if (dataInici < dataFinal){    
-            alert("DETALLS DEL CURS\n"+"Nom del curs: "+$("#nomCurs").val()+"\n"+
-            "Tipus de curs: "+$("#tipusCurso").val()+"\n"+
-            "Nom del professor: "+$("#nomProfessor").val()+"\n"+
+            alert("DETALLS DEL CURS\n"+"Nom del curs: "+$("#nomCurs").value()+"\n"+
+            "Tipus de curs: "+document.getElementById("#tipusCurso").value()+"\n"+
+            "Nom del professor: "+document.getElementById("#nomProfessor").value()+"\n"+
             "Data d'inici: "+dataIniciMostrar+"\n"+ 
             "Duració del curs en díes: "+dies+"\n"+
-            "Hores: "+$("#hores").val());
+            "Hores: "+document.getElementById("#hores").value());
             //Incluimos un reset para vaciar los campos del formulario 
             $("#formulari")[0].reset();
         }else{
